4a12673c5ec34cd4413fd8c182af36f0352dc9a2
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2021-09-22 13:10:47 -0500

    style landing page & check for session server side

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,8 +1,14 @@
-import { useSession, signIn, signOut } from 'next-auth/client'
+import { useSession, getSession, signIn, signOut } from 'next-auth/client'
 
-export default function App() {
-  const [session] = useSession()
+export async function getServerSideProps({ params, req, res }) {
+  const session = await getSession({ req })
 
+  return {
+    props: { session: session ? session : null },
+  }
+}
+
+export default function App({ session }) {
   return session ? (
     <div className="grid place-items-center h-screen">
       <div className="flex flex-col">
@@ -11,11 +17,22 @@ export default function App() {
       </div>
     </div>
   ) : (
-    <div className="grid place-items-center h-screen">
-      <div className="flex flex-col">
-        Not signed in <br />
-        <button onClick={() => signIn('github')}>Sign in</button>
-      </div>
-    </div>
+    <Landing />
+  )
+}
+
+function Landing() {
+  return (
+    <main className="h-screen w-screen flex flex-col justify-center items-center bg-red-800 space-y-6">
+      <p className="flex flex-col items-center text-gray-100 text-4xl">
+        dunwoody <span className="text-gray-900">software</span> engineering
+      </p>
+      <button
+        className="text-gray-900 hover:shadow-white font-bold transition bg-gray-100 rounded-xl py-2 px-4"
+        onClick={() => signIn('github')}
+      >
+        sign in
+      </button>
+    </main>
   )
 }
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        white: '1px 2px 8px 2px #ffffff90',
+      },
+    },
   },
   variants: {
     extend: {},
